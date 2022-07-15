01a900fb4b43cd77dba8f443bb544613196cfbb8
Layssa <oilayssaa@gmail.com>
2022-07-15 14:37:55 -0300

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const colorChange = document.getElementById('change-btn');
-const nameTextColor = document.getElementById('change-text');
+const colorChange = document.getElementById('change-btn')
+const nameTextColor = document.getElementById('change-text')
 const body = document.body;
+
+
+function randomColor(){
+    return Math.floor(Math.random()*255);
+}
+
+colorChange.addEventListener('click',bgColor)
+
+function bgColor(){
+    const color1 = randomColor();
+    const color2 = randomColor();
+    const color3 = randomColor();
+
+    const rgbString = `rgb(${color1},${color2},${color3})`
+
+    body.style.background = rgbString;
+    nameTextColor.innerText = rgbString;
+}
